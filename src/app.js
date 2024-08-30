dc35d1bb64aa7ce0b6ed7e69dcba717bc0de1fda
Tgodwin94 <tyler.godwin94@gmail.com>
2024-08-30 00:21:45 +0000

    TylerGodwin-DomainNameGenerator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const domainNameGenerator = () => {
+  const domains = document.querySelector("#domains");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let pronoun = ["all", "the", "our", "those", "that", "this"];
+  let adj = ["giant", "scary", "gooey", "hot", "fuzzy", "dark"];
+  let noun = ["people", "fish", "computer", "chair", "house", "shirt"];
+  let siteType = [".com", ".edu", ".gov", ".org", ".net"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < siteType.length; d++) {
+          domains.innerHTML +=
+            pronoun[a] + adj[b] + noun[c] + siteType[d] + " ";
+        }
+      }
+    }
+  }
 };
+
+window.onload = domainNameGenerator();
